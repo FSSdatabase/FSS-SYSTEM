dccbd6585bf9953b55dcfd80a58780fca47ef1d1
FSSdatabase <focusschools2@gmail.com>
2026-06-11 23:53:21 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/fss-system/",   // change to "/" if hosting on custom domain
+  base: "./",   // change to "/" if hosting on custom domain
 });
