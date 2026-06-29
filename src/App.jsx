ca984c467d71034265621870ec4fdb9770b76a2a
FSSdatabase <focusschools2@gmail.com>
2026-06-29 22:55:51 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
-import Layout         from "./components/Layout";
-import Login          from "./components/Login";
-import Dashboard      from "./pages/Dashboard";
-import Students       from "./pages/Students";
-import Attendance     from "./pages/Attendance";
-import DAAR           from "./pages/DAAR";
-import Fees           from "./pages/Fees";
-import Academics      from "./pages/Academics";
-import Reports        from "./pages/Reports";
-import Timetable      from "./pages/Timetable";
-import Manage         from "./pages/Manage";
+import Layout             from "./components/Layout";
+import Login              from "./components/Login";
+import Dashboard          from "./pages/Dashboard";
+import Students           from "./pages/Students";
+import Attendance         from "./pages/Attendance";
+import DAAR               from "./pages/DAAR";
+import Fees               from "./pages/Fees";
+import Academics          from "./pages/Academics";
+import Reports            from "./pages/Reports";
+import Timetable          from "./pages/Timetable";
+import Manage             from "./pages/Manage";
 import PlacementPromotion from "./pages/placementpromotion/placementpromotion";
-import QLUSDashboard  from "./pages/QLUSDashboard";
-import QuranTracker   from "./pages/QuranTracker";
-import TahfeezCenter  from "./pages/TahfeezCenter";
-import IslamicStudies from "./pages/IslamicStudies";
+import QLUSDashboard      from "./pages/QLUSDashboard";
+import QuranTracker       from "./pages/QuranTracker";
+import TahfeezCenter      from "./pages/TahfeezCenter";
+import IslamicStudies     from "./pages/IslamicStudies";
 
 export default function App() {
   const [page, setPage] = useState("dashboard");
@@ -42,20 +42,20 @@ export default function App() {
   };
 
   const pages = {
-    dashboard:      <Dashboard      setPage={setPage} user={user} />,
-    students:       <Students       />,
-    attendance:     <Attendance     />,
-    daar:           <DAAR           user={user} />,
-    fees:           <Fees           />,
-    academics:      <Academics      />,
-    reports:        <Reports        />,
-    timetable:      <Timetable      />,
-    manage:         <Manage         />,
+    dashboard:          <Dashboard          setPage={setPage} user={user} />,
+    students:           <Students           />,
+    attendance:         <Attendance         />,
+    daar:               <DAAR               user={user} />,
+    fees:               <Fees               />,
+    academics:          <Academics          />,
+    reports:            <Reports            />,
+    timetable:          <Timetable          />,
+    manage:             <Manage             />,
     placementpromotion: <PlacementPromotion />,
-    qlusdashboard:  <QLUSDashboard  setPage={setPage} />,
-    qurantracker:   <QuranTracker   />,
-    tahfeez:        <TahfeezCenter  />,
-    islamicstudies: <IslamicStudies />,
+    qlusdashboard:      <QLUSDashboard      setPage={setPage} />,
+    qurantracker:       <QuranTracker       />,
+    tahfeez:            <TahfeezCenter      />,
+    islamicstudies:     <IslamicStudies     />,
   };
 
   return (
